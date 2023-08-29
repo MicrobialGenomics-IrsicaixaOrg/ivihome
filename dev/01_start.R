cc43badfe994317dbed3edd1a7bb227158907f06
xec-cm <fcatala@irsicaixa.es>
2023-08-29 11:10:05 +0200

    Initial commit

--- dev/01_start.R
+++ dev/01_start.R
@@ -57,7 +57,7 @@ golem::use_recommended_tests()
 
 ## Favicon ----
 # If you want to change the favicon (default is golem's one)
-golem::use_favicon() # path = "path/to/ico". Can be an online file.
+golem::use_favicon("inst/app/www/favicon.ico") # path = "path/to/ico". Can be an online file.
 # golem::remove_favicon() # Uncomment to remove the default favicon
 
 ## Add helper functions ----
@@ -65,6 +65,7 @@ golem::use_utils_ui(with_test = TRUE)
 golem::use_utils_server(with_test = TRUE)
 
 # You're now set! ----
+build_readme()
 
 # go to dev/02_dev.R
 rstudioapi::navigateToFile("dev/02_dev.R")
